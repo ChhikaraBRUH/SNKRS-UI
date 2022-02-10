6a72d91dce59f319c3ff12c07572e5702599e63a
Chaitanya <67656388+ChhikaraBRUH@users.noreply.github.com>
2022-02-10 12:50:58 +0530

    fix: fixed js file

--- main.js
+++ main.js
@@ -24,20 +24,3 @@ const menuOpener = () => {
 
 navMenuBtn.addEventListener("click", menuOpener);
 navMenuCloseBtn.addEventListener("click", menuOpener);
-
-// Modal Functionality JS
-
-let modalOpenBtn = document.querySelector(".modal-open-btn");
-let modalContainer = document.querySelector(".modal-container");
-let modal = document.querySelector(".modal");
-let modalCloseBtn = document.getElementById("modal-close-btn");
-
-const openModal = () => (modalContainer.style.display = "flex");
-const closeModal = () => (modalContainer.style.display = "none");
-
-modalOpenBtn.addEventListener("click", openModal);
-modalCloseBtn.addEventListener("click", closeModal);
-modal.addEventListener("click", (e) => {
-	e.stopPropagation();
-});
-modalContainer.addEventListener("click", closeModal);
